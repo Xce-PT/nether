947a3bc69918d28731735ef5574181371b396772
João Santos <jps@xce.pt>
2023-07-27 15:31:56 +0100

    Added omni lights and diffuse reflections.

--- src/video/fb.rs
+++ src/video/fb.rs
@@ -13,9 +13,11 @@ use alloc::alloc::GlobalAlloc;
 use core::alloc::Layout;
 use core::iter::Iterator;
 use core::mem::size_of;
-use core::simd::{f32x4, mask32x4, u16x4, u16x8, u32x4, usizex8, SimdFloat, SimdPartialOrd, SimdUint};
+use core::simd::{f32x4, mask32x4, u16x4, u16x8, u32x4, usizex8, SimdFloat, SimdPartialEq, SimdPartialOrd, SimdUint};
+use core::slice::from_raw_parts as slice_from_raw_parts;
 use core::sync::atomic::{AtomicU64, Ordering};
 
+use super::shader::{Context, Light, Shader, Triangle};
 use crate::alloc::{Alloc, UNCACHED_REGION};
 use crate::simd::{SimdFloatExtra, SimdPartialEqExtra, SimdPartialOrdExtra};
 use crate::to_dma;
@@ -76,25 +78,10 @@ pub struct Tile<'a>
     // Axis aligned bounding box maximum values.
     max: f32x4,
     /// Tile's color buffer.
-    cb: Buffer,
+    cb: [u16x4; TILE_DIM_MAX * TILE_DIM_MAX / 4],
     /// Tile's depth buffer.
-    db: Buffer,
+    db: [u16x4; TILE_DIM_MAX * TILE_DIM_MAX / 4],
 }
-
-/// Vertex.
-#[derive(Clone, Copy, Debug)]
-pub struct Vertex
-{
-    /// Projected position.
-    pub proj: f32x4,
-    /// RGBA color.
-    pub color: f32x4,
-}
-
-/// Tile buffer.
-#[repr(align(0x40), C)]
-#[derive(Debug)]
-struct Buffer([u16; TILE_DIM_MAX * TILE_DIM_MAX]);
 
 impl FrameBuffer
 {
@@ -235,8 +222,8 @@ impl<'a> Tile<'a>
         let pty = f32x4::from_array([origy, origy, origy + sizey, origy + sizey]);
         let min = f32x4::from_array([origx, origy, 0.0, 0.0]);
         let max = f32x4::from_array([origx + sizex, origy + sizey, 1.0, f32::INFINITY]);
-        let cb = Buffer([0; TILE_DIM_MAX * TILE_DIM_MAX]);
-        let db = Buffer([0; TILE_DIM_MAX * TILE_DIM_MAX]);
+        let cb = [u16x4::splat(0); TILE_DIM_MAX * TILE_DIM_MAX / 4];
+        let db = [u16x4::splat(0); TILE_DIM_MAX * TILE_DIM_MAX / 4];
         Self { fb,
                col,
                row,
@@ -250,21 +237,20 @@ impl<'a> Tile<'a>
 
     /// Draws a triangle to the tile.
     ///
-    /// * `vert0`: First vertex.
-    /// * `vert1`: Second vertex.
-    /// * `vert2`: Third vertex.
-    pub fn draw_triangle(&mut self, vert0: Vertex, vert1: Vertex, vert2: Vertex)
+    /// * `tri`: Triangle to draw.
+    /// * `lights`: Lights potentially illuminating the triangle.
+    pub fn draw_triangle(&mut self, tri: &Triangle, lights: &[Light])
     {
         // Check whether the axis-aligned bounding boxes of the triangle and tile
         // overlap.
         let tmax = self.max;
-        let min = vert0.proj.simd_min(vert1.proj).simd_min(vert2.proj);
+        let min = tri.0.proj.simd_min(tri.1.proj).simd_min(tri.2.proj);
         if tmax.simd_lt(min).any() {
             // The triangle is completely outside this tile.
             return;
         }
         let tmin = self.min;
-        let max = vert0.proj.simd_max(vert1.proj).simd_max(vert2.proj);
+        let max = tri.0.proj.simd_max(tri.1.proj).simd_max(tri.2.proj);
         if tmin.simd_gt(max).any() {
             // The triangle is completely outside this tile.
             return;
@@ -272,12 +258,12 @@ impl<'a> Tile<'a>
         // Compute the linear barycentric coordinates at the corner control points.
         let ptx = self.ptx;
         let pty = self.pty;
-        let x0 = f32x4::splat(vert0.proj[0]) - ptx;
-        let y0 = f32x4::splat(vert0.proj[1]) - pty;
-        let x1 = f32x4::splat(vert1.proj[0]) - ptx;
-        let y1 = f32x4::splat(vert1.proj[1]) - pty;
-        let x2 = f32x4::splat(vert2.proj[0]) - ptx;
-        let y2 = f32x4::splat(vert2.proj[1]) - pty;
+        let x0 = f32x4::splat(tri.0.proj[0]) - ptx;
+        let y0 = f32x4::splat(tri.0.proj[1]) - pty;
+        let x1 = f32x4::splat(tri.1.proj[0]) - ptx;
+        let y1 = f32x4::splat(tri.1.proj[1]) - pty;
+        let x2 = f32x4::splat(tri.2.proj[0]) - ptx;
+        let y2 = f32x4::splat(tri.2.proj[1]) - pty;
         let area0 = x1 * y2 - x2 * y1;
         if area0.reduce_max() < 0.0 {
             // The triangle is completely outside this tile.
@@ -351,12 +337,12 @@ impl<'a> Tile<'a>
                 let tminy = f32x4::splat(tmin[1]);
                 let tmaxx = f32x4::splat(tmax[0]);
                 let tmaxy = f32x4::splat(tmax[1]);
-                let vx0 = f32x4::splat(vert0.proj[0]);
-                let vy0 = f32x4::splat(vert0.proj[1]);
-                let vx1 = f32x4::splat(vert1.proj[0]);
-                let vy1 = f32x4::splat(vert1.proj[1]);
-                let vx2 = f32x4::splat(vert2.proj[0]);
-                let vy2 = f32x4::splat(vert2.proj[1]);
+                let vx0 = f32x4::splat(tri.0.proj[0]);
+                let vy0 = f32x4::splat(tri.0.proj[1]);
+                let vx1 = f32x4::splat(tri.1.proj[0]);
+                let vy1 = f32x4::splat(tri.1.proj[1]);
+                let vx2 = f32x4::splat(tri.2.proj[0]);
+                let vy2 = f32x4::splat(tri.2.proj[1]);
                 let valid0 = x0.simd_ge(tminx) & x0.simd_le(tmaxx) & y0.simd_ge(tminy) & y0.simd_le(tmaxy);
                 let valid0 = valid0 & x0.simd_ge(vx1.simd_min(vx2)) & x0.simd_le(vx1.simd_max(vx2));
                 let valid0 = valid0 & y0.simd_ge(vy1.simd_min(vy2)) & y0.simd_le(vy1.simd_max(vy2));
@@ -375,8 +361,8 @@ impl<'a> Tile<'a>
                 let y2 = valid2.select(y2, nan);
                 let x3 = intc.select(ptx, nan);
                 let y3 = intc.select(pty, nan);
-                let x = f32x4::from_array([vert0.proj[0], vert1.proj[0], vert2.proj[0], f32::NAN]);
-                let y = f32x4::from_array([vert0.proj[1], vert1.proj[1], vert2.proj[1], f32::NAN]);
+                let x = f32x4::from_array([tri.0.proj[0], tri.1.proj[0], tri.2.proj[0], f32::NAN]);
+                let y = f32x4::from_array([tri.0.proj[1], tri.1.proj[1], tri.2.proj[1], f32::NAN]);
                 let tminx = f32x4::splat(tmin[0]);
                 let tminy = f32x4::splat(tmin[1]);
                 let tmaxx = f32x4::splat(tmax[0]);
@@ -455,7 +441,8 @@ impl<'a> Tile<'a>
         let gmul = f32x4::splat(63.5);
         let rshift = u32x4::splat(11);
         let gshift = u32x4::splat(5);
-        let project = vert0.proj[3] != vert1.proj[3] || vert0.proj[3] != vert2.proj[3];
+        let is_affine = tri.0.proj[3] == tri.1.proj[3] && tri.0.proj[3] == tri.2.proj[3];
+        let is_plane = tri.0.normal.simd_eq(tri.1.normal).all() && tri.0.normal.simd_eq(tri.2.normal).all();
         // Loop over all the fragments in the tile in groups of 2x2, and shade those
         // that belong to the triangle.
         let mut vbary0 = bary0;
@@ -481,34 +468,35 @@ impl<'a> Tile<'a>
                     hbary2 += hinc2;
                     continue;
                 }
-                let (bary0, bary1, bary2) = if project {
-                    // Compute the perspective-correct barycentric coordinates.
-                    let w0 = bary0.mul_lane::<3>(vert0.proj);
-                    let w1 = bary1.mul_lane::<3>(vert1.proj);
-                    let w2 = bary2.mul_lane::<3>(vert2.proj);
-                    let itotal = (w0 + w1 + w2).fast_recip();
-                    let bary0 = w0 * itotal;
-                    let bary1 = w1 * itotal;
-                    let bary2 = w2 * itotal;
-                    (bary0, bary1, bary2)
-                } else {
+                let ctx = if is_affine {
                     // Affine projection.
-                    (hbary0, hbary1, hbary2)
+                    Context { bary0: hbary0,
+                              bary1: hbary1,
+                              bary2: hbary2,
+                              is_plane }
+                } else {
+                    // Compute the perspective-correct barycentric coordinates.
+                    let w0 = bary0.mul_lane::<3>(tri.0.proj);
+                    let w1 = bary1.mul_lane::<3>(tri.1.proj);
+                    let w2 = bary2.mul_lane::<3>(tri.2.proj);
+                    let itotal = (w0 + w1 + w2).fast_recip();
+                    Context { bary0: w0 * itotal,
+                              bary1: w1 * itotal,
+                              bary2: w2 * itotal,
+                              is_plane }
                 };
                 // Offset for these 4 fragments in the tile buffers.
-                let offset = (trow >> 1) * (twidth << 1) + (tcol << 1);
+                let offset = (trow >> 1) * (twidth >> 1) + (tcol >> 1);
                 // Compute the depth and exclude all fragments outside the range between the
                 // values in the depth buffer and the near clipping plane.
-                let db = unsafe { self.db.0.as_mut_ptr().add(offset).cast::<u16x4>() };
-                let odepth = unsafe { db.read() };
-                let z = bary0.mul_lane::<2>(vert0.proj);
-                let z = z.fused_mul_add_lane::<2>(bary1, vert1.proj);
-                let z = z.fused_mul_add_lane::<2>(bary2, vert2.proj);
-                valid &= z.simd_le(one);
-                let zb = z.to_bits().saturating_sub(dxb);
-                let zx = (zb & dxm) >> ds;
-                let zm = (zb & dmm) >> ds;
-                let depth = (zx | zm).cast::<u16>();
+                let odepth = self.db[offset];
+                let mut shader = Shader::new(tri, ctx);
+                let depth = shader.depth();
+                valid &= depth.simd_le(one);
+                let depthb = depth.to_bits().saturating_sub(dxb);
+                let depthx = (depthb & dxm) >> ds;
+                let depthm = (depthb & dmm) >> ds;
+                let depth = (depthx | depthm).cast::<u16>();
                 let mut valid = valid.cast::<i16>();
                 valid &= depth.simd_gt(odepth);
                 if !valid.any() {
@@ -518,35 +506,26 @@ impl<'a> Tile<'a>
                     hbary2 += hinc2;
                     continue;
                 }
-                // Store the new depth values.
-                let depth = valid.select(depth, odepth);
-                unsafe { db.write(depth) };
+                self.db[offset] = valid.select(depth, odepth);
                 // Apply shading.
-                let cb = unsafe { self.cb.0.as_mut_ptr().add(offset).cast::<u16x4>() };
-                let ocolor = unsafe { cb.read() };
-                let red = bary0.mul_lane::<0>(vert0.proj);
-                let red = red.fused_mul_add_lane::<0>(bary1, vert1.proj);
-                let red = red.fused_mul_add_lane::<0>(bary2, vert2.proj);
-                let red = red.simd_max(zero).simd_min(one);
-                let green = bary0.mul_lane::<1>(vert0.proj);
-                let green = green.fused_mul_add_lane::<1>(bary1, vert1.proj);
-                let green = green.fused_mul_add_lane::<1>(bary2, vert2.proj);
-                let green = green.simd_max(zero).simd_min(one);
-                let blue = bary0.mul_lane::<2>(vert0.proj);
-                let blue = blue.fused_mul_add_lane::<2>(bary1, vert1.proj);
-                let blue = blue.fused_mul_add_lane::<2>(bary2, vert2.proj);
-                let blue = blue.simd_max(zero).simd_min(one);
+                lights.iter().for_each(|l| shader.illuminate(l));
+                let (red, green, blue) = shader.finish();
                 // Compute the RGB565 color values.
+                let ocolor = self.cb[offset];
+                let red = red.simd_max(zero).simd_min(one);
+                let green = green.simd_max(zero).simd_min(one);
+                let blue = blue.simd_max(zero).simd_min(one);
                 let red = (red * rbmul).cast::<u32>() << rshift;
                 let green = (green * gmul).cast::<u32>() << gshift;
                 let blue = (blue * rbmul).cast::<u32>();
                 let color = (red | green | blue).cast::<u16>();
-                let color = valid.select(color, ocolor);
-                unsafe { cb.write(color) };
+                self.cb[offset] = valid.select(color, ocolor);
+                // Apply horizontal increments.
                 hbary0 += hinc0;
                 hbary1 += hinc1;
                 hbary2 += hinc2;
             }
+            // Apply vertical increments.
             vbary0 += vinc0;
             vbary1 += vinc1;
             vbary2 += vinc2;
@@ -567,13 +546,17 @@ impl<'a> Drop for Tile<'a>
         let eindices = usizex8::from_array([0, 1, 4, 5, 8, 9, 12, 13]);
         let oindices = usizex8::from_array([2, 3, 6, 7, 10, 11, 14, 15]);
         let black = u16x8::splat(0);
-        for trow in 0 .. self.fb.theight {
+        let twidth = self.fb.twidth;
+        let theight = self.fb.theight;
+        let width = self.fb.width;
+        for trow in 0 .. theight {
             let indices = if trow & 0x1 == 0 { eindices } else { oindices };
-            let buf = unsafe { buf.add(trow * self.fb.width) };
-            for tcol in (0 .. self.fb.twidth).step_by(8) {
-                let offset = usizex8::splat((self.fb.twidth << 1) * (trow >> 1) + (tcol << 1));
+            let buf = unsafe { buf.add(trow * width) };
+            let cb = unsafe { slice_from_raw_parts(self.cb.as_ptr().cast::<u16>(), TILE_DIM_MAX * TILE_DIM_MAX) };
+            for tcol in (0 .. twidth).step_by(8) {
+                let offset = usizex8::splat((trow >> 1) * (twidth << 1) + (tcol << 1));
                 let indices = indices + offset;
-                let color = u16x8::gather_or(&self.cb.0[..], indices, black);
+                let color = u16x8::gather_or(cb, indices, black);
                 unsafe { buf.add(tcol).cast::<u16x8>().write(color) };
             }
         }
--- src/video/geom.rs
+++ src/video/geom.rs
@@ -2,76 +2,140 @@
 //!
 //! Contains geometry generation functionality.
 
-use core::f32::consts::FRAC_PI_3;
-
 use super::*;
-use crate::math::Angle;
 
-/// Linearly interpolated color triangle.
+/// Rainbow cube.
 #[derive(Debug)]
-pub struct Triangle
+pub struct Cube
 {
     /// Geometry.
-    geom: [Vertex; 6],
+    geom: [Triangle; 12],
 }
 
-/// Gray background square.
-pub struct Square
+impl Cube
 {
-    /// Geometry.
-    geom: [Vertex; 6],
-}
-
-impl Triangle
-{
-    /// Creates and initializes a new triangle.
+    /// Creates and initializes a new rainbow cube.
     ///
-    /// Returns the newly created triangle.
+    /// Returns the newly created cube.
     pub fn new() -> Self
     {
-        let tan = Angle::from(FRAC_PI_3).tan();
-        let vert0 = f32x4::from_array([-1.0, -tan / 3.0, 0.0, 1.0]);
-        let vert1 = f32x4::from_array([1.0, -tan / 3.0, 0.0, 1.0]);
-        let vert2 = f32x4::from_array([0.0, tan / 3.0 * 2.0, 0.0, 1.0]);
-        let vert0 = Vertex { pos: vert0,
-                             color: f32x4::from_array([1.0, 0.0, 0.0, 1.0]) };
-        let vert1 = Vertex { pos: vert1,
-                             color: f32x4::from_array([0.0, 0.0, 1.0, 1.0]) };
-        let vert2 = Vertex { pos: vert2,
-                             color: f32x4::from_array([0.0, 1.0, 0.0, 1.0]) };
-        let geom = [vert0, vert1, vert2, vert2, vert1, vert0];
+        // Vertices.
+        let vbdl = f32x4::from_array([-1.0, -1.0, -1.0, 1.0]);
+        let vbdr = f32x4::from_array([1.0, -1.0, -1.0, 1.0]);
+        let vbul = f32x4::from_array([-1.0, 1.0, -1.0, 1.0]);
+        let vbur = f32x4::from_array([1.0, 1.0, -1.0, 1.0]);
+        let vfdl = f32x4::from_array([-1.0, -1.0, 1.0, 1.0]);
+        let vfdr = f32x4::from_array([1.0, -1.0, 1.0, 1.0]);
+        let vful = f32x4::from_array([-1.0, 1.0, 1.0, 1.0]);
+        let vfur = f32x4::from_array([1.0, 1.0, 1.0, 1.0]);
+        // Normals.
+        let nl = f32x4::from_array([-1.0, 0.0, 0.0, 0.0]);
+        let nr = f32x4::from_array([1.0, 0.0, 0.0, 0.0]);
+        let nd = f32x4::from_array([0.0, -1.0, 0.0, 0.0]);
+        let nu = f32x4::from_array([0.0, 1.0, 0.0, 0.0]);
+        let nb = f32x4::from_array([0.0, 0.0, -1.0, 0.0]);
+        let nf = f32x4::from_array([0.0, 0.0, 1.0, 0.0]);
+        // Colors.
+        let cbdl = f32x4::from_array([0.0, 0.0, 0.0, 1.0]);
+        let cbdr = f32x4::from_array([1.0, 0.0, 0.0, 1.0]);
+        let cbul = f32x4::from_array([0.0, 0.0, 1.0, 1.0]);
+        let cbur = f32x4::from_array([1.0, 0.0, 1.0, 1.0]);
+        let cfdl = f32x4::from_array([0.0, 1.0, 0.0, 1.0]);
+        let cfdr = f32x4::from_array([1.0, 1.0, 0.0, 1.0]);
+        let cful = f32x4::from_array([0.0, 1.0, 1.0, 1.0]);
+        let cfur = f32x4::from_array([1.0, 1.0, 1.0, 1.0]);
+        // Cube faces.
+        let fb0 = Vertex { pos: vbdl,
+                           normal: nb,
+                           color: cbdl };
+        let fb1 = Vertex { pos: vbul,
+                           normal: nb,
+                           color: cbul };
+        let fb2 = Vertex { pos: vbdr,
+                           normal: nb,
+                           color: cbdr };
+        let fb3 = Vertex { pos: vbur,
+                           normal: nb,
+                           color: cbur };
+        let ff0 = Vertex { pos: vfdr,
+                           normal: nf,
+                           color: cfdr };
+        let ff1 = Vertex { pos: vfur,
+                           normal: nf,
+                           color: cfur };
+        let ff2 = Vertex { pos: vfdl,
+                           normal: nf,
+                           color: cfdl };
+        let ff3 = Vertex { pos: vful,
+                           normal: nf,
+                           color: cful };
+        let fl0 = Vertex { pos: vfdl,
+                           normal: nl,
+                           color: cfdl };
+        let fl1 = Vertex { pos: vful,
+                           normal: nl,
+                           color: cful };
+        let fl2 = Vertex { pos: vbdl,
+                           normal: nl,
+                           color: cbdl };
+        let fl3 = Vertex { pos: vbul,
+                           normal: nl,
+                           color: cbul };
+        let fr0 = Vertex { pos: vbdr,
+                           normal: nr,
+                           color: cbdr };
+        let fr1 = Vertex { pos: vbur,
+                           normal: nr,
+                           color: cbur };
+        let fr2 = Vertex { pos: vfdr,
+                           normal: nr,
+                           color: cfdr };
+        let fr3 = Vertex { pos: vfur,
+                           normal: nr,
+                           color: cfur };
+        let fd0 = Vertex { pos: vbdr,
+                           normal: nd,
+                           color: cbdr };
+        let fd1 = Vertex { pos: vfdr,
+                           normal: nd,
+                           color: cfdr };
+        let fd2 = Vertex { pos: vbdl,
+                           normal: nd,
+                           color: cbdl };
+        let fd3 = Vertex { pos: vfdl,
+                           normal: nd,
+                           color: cfdl };
+        let fu0 = Vertex { pos: vfur,
+                           normal: nu,
+                           color: cfur };
+        let fu1 = Vertex { pos: vbur,
+                           normal: nu,
+                           color: cbur };
+        let fu2 = Vertex { pos: vful,
+                           normal: nu,
+                           color: cful };
+        let fu3 = Vertex { pos: vbul,
+                           normal: nu,
+                           color: cbul };
+        // Cube triangles.
+        let t0 = Triangle(fb0, fb1, fb2);
+        let t1 = Triangle(fb2, fb1, fb3);
+        let t2 = Triangle(ff0, ff1, ff2);
+        let t3 = Triangle(ff2, ff1, ff3);
+        let t4 = Triangle(fl0, fl1, fl2);
+        let t5 = Triangle(fl2, fl1, fl3);
+        let t6 = Triangle(fr0, fr1, fr2);
+        let t7 = Triangle(fr2, fr1, fr3);
+        let t8 = Triangle(fu0, fu1, fu2);
+        let t9 = Triangle(fu2, fu1, fu3);
+        let t10 = Triangle(fd0, fd1, fd2);
+        let t11 = Triangle(fd2, fd1, fd3);
+        let geom = [t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11];
         Self { geom }
     }
 
     /// Returns the geometry of the triangle.
-    pub fn geom(&self) -> &[Vertex]
-    {
-        &self.geom
-    }
-}
-
-impl Square
-{
-    /// Creates and initializes a new square.
-    ///
-    /// Returns the newly created square.
-    pub fn new() -> Self
-    {
-        let vert0 = f32x4::from_array([-1.0, -1.0, 0.0, 1.0]);
-        let vert1 = f32x4::from_array([1.0, -1.0, 0.0, 1.0]);
-        let vert2 = f32x4::from_array([-1.0, 1.0, 0.0, 1.0]);
-        let vert3 = f32x4::from_array([1.0, 1.0, 0.0, 1.0]);
-        let color = f32x4::from_array([0.5, 0.5, 0.5, 1.0]);
-        let vert0 = Vertex { pos: vert0, color };
-        let vert1 = Vertex { pos: vert1, color };
-        let vert2 = Vertex { pos: vert2, color };
-        let vert3 = Vertex { pos: vert3, color };
-        let geom = [vert0, vert1, vert2, vert2, vert1, vert3];
-        Self { geom }
-    }
-
-    /// Returns the geometry of the square.
-    pub fn geom(&self) -> &[Vertex]
+    pub fn geom(&self) -> &[Triangle]
     {
         &self.geom
     }
